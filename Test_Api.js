aaffc43e9f75e3e672cfcdbfe10b71f3735abd1f
leedh909 <leedh909@naver.com>
2021-01-29 13:28:36 +0900

    API 데이터 출력

--- Test_Api.js
+++ Test_Api.js
@@ -16,7 +16,6 @@ export default class App extends React.Component {
             isLoading: false,
             dataSource: responseJson.movies,
           },
-          function() {}
         );
       })
       .catch(error => {
